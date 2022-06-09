e60393ce4cf8cb573cacfc29171a8812fb59d5e1
Augusta-Satrianto <107111227+Augusta-Satrianto@users.noreply.github.com>
2022-06-09 13:38:13 +0700

    Update edit-recipe.blade.php

--- resources/views/edit-recipe.blade.php
+++ resources/views/edit-recipe.blade.php
@@ -2,16 +2,16 @@
 
 @section('container')
 
-    <div class="container-akun" style="background:#F9F1DE;margin:0 auto 50px;border-radius:15px;width:58%;padding:20px;min-width: 400px;">
+    <div class="container-recipe">
         <form action="/edit/{{$post->slug}}" method="post" enctype="multipart/form-data">
             @csrf
-            <ul style="list-style-type:none;">
-                <li style="">
+            <ul>
+                <li>
                     <label for="picture" ></label>
-                        <div style="margin:0 auto;width:300px;max-height:200px;border-radius: 23px;overflow:hidden;">
-                            <img class="img-preview" src="{{ asset('storage/' . $post->picture) }}"  alt="" style="width:300px;">
+                        <div class="picture-default">
+                            <img class="img-preview" src="{{ asset('storage/' . $post->picture) }}"  alt="">
                         </div>
-                    <div style="margin:10px auto;width:300px;">
+                    <div class="picture-preview">
                         <input class="@error('picture')is-invalid @enderror" type="file" name="picture" id="picture" onchange="previewImage()">
                     </div>
                     @error('picture')
@@ -21,19 +21,19 @@
                     @enderror
                 </li>
                 <li>
-                    <label for="title" style="color: #EAB141;font-size:18px;font-family:'Montserrat', sans-serif;">Judul :</label>
+                    <label class="label-write" for="title">Judul :</label>
                     <input type="text" name="title" id="title" autocomplete="off" required value="{{ old('title', $post->title) }}">
                 </li>
                 <li>
-                    <label for="ingredient" style="color: #EAB141;font-size:18px;font-family:'Montserrat', sans-serif;">Bahan-bahan</label><br>
-                    <textarea type="text" name="ingredient" id="ingredient" rows="10" cols="50" required style="overflow: auto;resize: none;">{{ $post->ingredient }}</textarea>
+                    <label class="label-write" for="ingredient">Bahan-bahan</label><br>
+                    <textarea type="text" name="ingredient" id="ingredient" rows="10" cols="50" required autocomplete="off">{{ $post->ingredient }}</textarea>
                 </li>
                 <li>
-                    <label for="step" style="color: #EAB141;font-size:18px;font-family:'Montserrat', sans-serif;">Langkah-langkah</label><br>
-                    <textarea type="text" name="step" id="step" rows="10" cols="50" required style="overflow: auto;resize: none;">{{ old('step', $post->step) }}</textarea>
+                    <label class="label-write" for="step">Langkah-langkah</label><br>
+                    <textarea type="text" name="step" id="step" rows="10" cols="50" required autocomplete="off">{{ old('step', $post->step) }}</textarea>
                 </li>
                 <li>
-                    <label for="category_id" style="color: #EAB141;font-size:18px;font-family:'Montserrat', sans-serif;">Kategori : </label>
+                    <label class="label-write" for="category_id">Kategori : </label>
                     <select id="category_id" name="category_id">
                         @foreach ($categories as $category)
                             @if(old('category_id', $post->category_id ) == $category->id)
@@ -45,7 +45,7 @@
                     </select>
                 </li>
                 <li>
-                    <div style="margin:30px auto 0;width:155px;">
+                    <div class="submit-recipe">
                         <button type="submit" name="submit" id="upload-resep">Upload Recipe</button> 
                     </div>         
                 </li>
