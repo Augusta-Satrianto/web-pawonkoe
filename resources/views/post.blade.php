44f75a33e95e89911cbea76704548c68d53fc26d
Augusta-Satrianto <107111227+Augusta-Satrianto@users.noreply.github.com>
2022-06-09 13:39:26 +0700

    Update post.blade.php

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,45 +2,47 @@
 
 @section('container')
     
-    <div class="container-2 " style="background:#F9F1DE;margin:0 auto;border-radius:15px;width:60%;padding:30px 20px 10px 20px;min-width: 400px;transform: translate(0,-40px);height:70px;">
-        <a href="/edit/{{ $post->slug}}"><div class="edit" style="width:69px;background-color:#EAB141;float:right;border-radius:5px;text-align:center;line-height:27px;color:white;float:left;">Edit</div></a> 
-        <a href="/edit/{{ $post->slug}}">
-            <div class="edit" style="width:140px;background-color:#EAB141;float:right;border-radius:5px;text-align:center;line-height:27px;color:white;float:right;">
+    <div class="container-post-atas">
+        <a href="/edit/{{ $post->slug}}"><div class="edit">Edit</div></a> 
+        <a href="/delete/{{ $post->slug}}">
+            <div class="delete">
                 <i class="bi bi-trash-fill"> </i>Delete Recipe
             </div>
         </a>
     </div>
     
-    <div class="container-2" style="background:#F9F1DE;margin:0 auto 30px;border-radius:15px;width:60%;padding-bottom:10px;min-width: 400px;transform: translate(0,-30px);">
-        <div style="width:322px;margin: 0 auto;padding-top:20px;">
-            <div style="width:322px;height:230px;border-radius: 23px;overflow:hidden;">
-                <img src="{{ asset('storage/' . $post->picture) }}" alt="" style="width:322px;">
+    <div class="container-post-bawah">
+        <div class="post-title">
+            <div class="picture-post">
+                <img src="{{ asset('storage/' . $post->picture) }}" alt="">
             </div>  
-            <p style="font-size:24px;margin:10px 0;color:#EAB141;text-align: center;max-width: 100%;overflow:hidden;">{{ $post->title }}</p>       
+            <p>{{ $post->title }}</p>       
         </div>
 
-        <hr style="height:2px;color:#EAB141;">
-        <div style="width:80%;margin:10px auto;">
-            <div style="color:#EAB141;font-size:20px;font-family:'Montserrat', sans-serif;margin-bottom:10px;">Bahan-bahan</div>
-            <div style="font-weight:500;line-height:20px;color:#7D7B75;font-size:16px;white-space: pre-line;max-width: 100%;overflow:hidden;font-family:'Montserrat',sans-serif;">{{ $post->ingredient }}</div>
+        <hr>
+        <div class="isi">
+            <div class="sub-isi">Bahan-bahan</div>
+            <div class="text-sub-isi">{{ $post->ingredient }}</div>
         </div>
 
-        <hr style="height:2px;color:#EAB141;">
-        <div style="width:80%;margin:10px auto;">
-            <div style="color:#EAB141;font-size:20px;font-family:'Montserrat', sans-serif;margin-bottom:10px;">Langkah-langkah</div>
-            <div style="font-weight:500;line-height:20px;color:#7D7B75;font-size:16px;white-space: pre-line;max-width: 100%;overflow:hidden;font-family:'Montserrat',sans-serif;">{{ $post->step }}</div>
+        <hr>
+        <div class="isi" class="isi">
+            <div class="sub-isi">Langkah-langkah</div>
+            <div class="text-sub-isi">{{ $post->step }}</div>
         </div>
 
-        <hr style="height:2px;color:#EAB141;">
+        <hr>
         <div class="text-center">
-            <img class="mb-4 rounded-circle" src="{{ asset('storage/' . $post->user->photo) }}" width="149" top="50">
-            <div style="font-size:24px;font-family:'Montserrat', sans-serif;color:#EAB141">Published By</div>
-            <div style="font-size:16px;font-family:'Montserrat', sans-serif;color:#000000;overflow:hidden;">{{ $post->user->username }}
+            <div class="post-user-photo">
+                <img class="mb-4" src="{{ asset('storage/' . $post->user->photo) }}" top="50">
+            </div>        
+            <p>Published By</p>
+            <div class="post-user-username">{{ $post->user->username }}
                 @if($post->user->isVerified)
-                    <i class="bi bi-patch-check-fill" style="color: #5C93FF;margin-left: 1px;"></i>
+                    <i class="bi bi-patch-check-fill"></i>
                 @endif
             </div>
-            <div style="font-size:16px;font-family:'Montserrat', sans-serif;color:#EAB141;">{{ $post->updated_at->format('M, d Y') }}</div>
+            <div class="tanggal">{{ $post->updated_at->format('M, d Y') }}</div>
         </div>
     </div>
     
